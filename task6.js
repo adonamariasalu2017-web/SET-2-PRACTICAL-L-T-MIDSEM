938666d91897ed0e67fe30c2b32d0433153f18e7
Adona <adonamariasalu2017@gmail.com>
2026-08-31 10:01:00 +0530

    Create task6.js for Countdown App with Nodemon
    Add initial implementation of Countdown App with Nodemon support.

---
+++ task6.js
@@ -0,0 +1,5 @@
+// This program can be run with Nodemon.
+// Nodemon automatically restarts the application
+// whenever changes are made and the file is saved.
+
+console.log("Countdown App is running with Nodemon!");
